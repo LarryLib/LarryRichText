8bd5a364a40b3f0dfcc3c64e8c863d94f350e239
lxd-Super-main <lxd_super_main@126.com>
2018-05-25 22:56:36 +0800

    版本一

---
+++ LarryRichText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LarryRichText
+//
+//  Created by Larry Mac Pro on 2018/5/25.
+//  Copyright © 2018年 LarryTwofly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LarryRichText/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LarryRichText
+//
+//  Created by Larry Mac Pro on 2018/5/25.
+//  Copyright © 2018年 LarryTwofly. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
